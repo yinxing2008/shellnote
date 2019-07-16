2ece5cf12f657bf1ab5a86600d2c12cbf4f4d1f6
jerry <309032663@qq.com>
2019-07-16 09:28:35 +0800

    优化token失效处理代码

--- client/kotlin/app/src/main/java/com/cxyzy/note/network/interceptor/TokenInterceptor.kt
+++ client/kotlin/app/src/main/java/com/cxyzy/note/network/interceptor/TokenInterceptor.kt
@@ -4,27 +4,33 @@ import com.cxyzy.note.HttpCode.TOKEN_EXPIRED
 import com.cxyzy.note.ext.replaceTokenInHeader
 import com.cxyzy.note.network.LoginManager
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 
 class TokenInterceptor : BaseInterceptor() {
     override fun interceptMe(chain: Interceptor.Chain): Response {
         val request = chain.request()
-        var response: Response? = null
-        try {
-            response = chain.proceed(request)
-            if (isTokenExpired(response!!)) {
-                LoginManager.login(
-                        onSuccess = {
-                            val newRequest = replaceTokenInHeader(request, it.token)
-                            response?.close()
-                            response = chain.proceed(newRequest)
-                        }
-                )
-            }
-        } catch (e: Exception) {
+        var response = chain.proceed(request)
+        if (isTokenExpired(response)) {
+            response = loginAndReSendOriginalRequest(request, response, chain)
         }
 
-        return response!!
+        return response
+    }
+
+    /**
+     * 重新登陆，并再次发送之前的请求
+     */
+    private fun loginAndReSendOriginalRequest(request: Request, originalResponse: Response, chain: Interceptor.Chain): Response {
+        var response = originalResponse
+        LoginManager.login(
+                onSuccess = {
+                    val newRequest = replaceTokenInHeader(request, it.token)
+                    response?.close()
+                    response = chain.proceed(newRequest)
+                }
+        )
+        return response
     }
 
     private fun isTokenExpired(response: Response) = (response.code() == TOKEN_EXPIRED)
